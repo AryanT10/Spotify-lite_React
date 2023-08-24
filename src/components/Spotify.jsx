832ac4119375ac647174a5829bd34ce968a06db5
atuwar@myseneca.ca <atuwar@myseneca.ca>
2023-08-24 15:32:45 -0400

    Feat: Made the login page

---
+++ src/components/Spotify.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components';
+
+export default function Spotify() {
+	return (
+		<Container>
+			<div className="spotify__body">
+				<div className="body">
+
+				</div>
+			</div>
+		</Container>
+	)
+}
+
+const Container = styled.div``;
