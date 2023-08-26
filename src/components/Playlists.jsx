9ccade7be2431c9ebd6e1560542a990e428e3125
atuwar@myseneca.ca <atuwar@myseneca.ca>
2023-08-25 21:27:20 -0400

    feat: change playlist and content onClick

--- src/components/Playlists.jsx
+++ src/components/Playlists.jsx
@@ -23,6 +23,9 @@ export default function Playlists() {
 		getPlaylistData();
 	}, [token, dispatch])
 
+	const changeCurrentPlaylist = (selectedPlaylistId) => {
+		dispatch({type:reducerCases.SET_PLAYLIST_ID,selectedPlaylistId})
+	}
 
 	return (
 		<Container>
@@ -30,7 +33,7 @@ export default function Playlists() {
 				{
 					playlists.map(({ name, id }) => {
 						return (
-							<li key={id}>{name}</li>
+							<li key={id} onClick={()=>changeCurrentPlaylist(id)}>{name}</li>
 						)
 					})
 				}
